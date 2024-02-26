40e7df2e7a7cf32c59f28c76c9a0d71098565cae
Horvath <Balazs.Horvath@xerox.com>
2024-02-26 10:41:52 +0000

    m

--- wpsProject/settings.py
+++ wpsProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-2@16-x8wtlq-q)gavhyk4+n5)k93(sq9t9ojn__wd0vb-u#oc5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
